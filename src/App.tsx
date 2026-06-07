6d83eb8a1b8f75d0a1e09c2269254e8720f0e563
dmm.stranger <molla.dmm@gmail.com>
2026-06-07 23:51:18 +0600

    add demo folder for show project scafolding files: base layout + project architecture

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <section id="spacer">
-        <h1 className="text-3xl text-green-600 font-bold underline">
+        <h1 className="text-3xl text-red-600 font-bold">
           Hello world!
         </h1>
       </section>
