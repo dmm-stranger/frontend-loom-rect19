a273ac21b491fd5f311f19b42ba61bd6a15cc811
dmm.stranger <molla.dmm@gmail.com>
2026-06-07 22:11:51 +0600

    setup loom project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="spacer">
+        <h1 className="text-3xl text-green-600 font-bold underline">
+          Hello world!
+        </h1>
+      </section>
+    </>
+  )
+}
+
+export default App
